ed35ab58fb843a251ae7ed0851916b05cb6c2df9
afreshnock <picklefish01@gmail.com>
2023-11-17 12:45:51 -0600

    tile collison

--- Lab5b/gsensor/Tiles.c
+++ Lab5b/gsensor/Tiles.c
@@ -1,5 +1,11 @@
 #include "VGA.h"
 
+#define TILE_HEIGHT 20
+#define TILE_WIDTH 20
+#define MAP_HEIGHT 32
+#define MAP_WIDTH 24
+
+
 int tiles [768] = {2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,
 2,1,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,2,
 2,1,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,2,
@@ -25,6 +31,8 @@ int tiles [768] = {2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2
 2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,
 2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2 };
 
+int tileIndexArray[9] = {0,0,0,0,0,0,0,0,0};
+
 
 void DrawTiles(void *virtual_base){
     int i=0; 
@@ -46,7 +54,69 @@ void DrawTiles(void *virtual_base){
     }
 }
 
-void TileCollision(int *x, int *y)
+void TileCollision(int *Player_x, int *Player_y)
 {
-    
+    int playerIndex = Player_y / TILE_HEIGHT * MAP_WIDTH) + (Player_x / TILE_WIDTH);
+            //int playerindex = _tilemap.TileIndices[index] - 1;
+            tileIndexArray[0] = playerIndex - 1 - MAP_WIDTH;
+            tileIndexArray[1] = playerIndex - MAP_WIDTH;
+            tileIndexArray[2] = playerIndex - MAP_WIDTH + 1;
+            tileIndexArray[3] = playerIndex - 1;
+            tileIndexArray[4] = playerIndex;
+            tileIndexArray[5] = playerIndex + 1;
+            tileIndexArray[6] = playerIndex + MAP_WIDTH - 1;
+            tileIndexArray[7] = playerIndex + MAP_WIDTH;
+            tileIndexArray[8] = playerIndex + MAP_WIDTH + 1;
+
+            
+            //[0][1][2]
+            //[3][*][5]
+            //[6][7][8]
+
+            int i;
+            for (i = 0; i < 9; i++)
+            {
+                if (tileIndexArray[i] > -1 && tileIndexArray[i] < MAP_HEIGHT * MAP_WIDTH)
+                {
+                    switch(tiles[tileIndexArray[i]])
+                    {
+                        case 1:
+                        //air tile do nothing
+                        break;
+
+                        case 2: // 2 is wall tile
+                        switch(i)
+                        {
+                            case 1: // top tile
+                                if(*Player_y <= ( (tileIndexArray[i] / MAP_WIDTH) + 1 ) * TILE_HEIGHT){
+                                    *Player_y = (tileIndexArray[i] / MAP_WIDTH) + 1 ) * TILE_HEIGHT;
+                                }
+
+                            break;
+                            case 3: //left tile
+                            break;
+                            case 5: // right tile
+                            break;
+                            case 7: 
+                            break;
+                            default:
+                            //do nothing
+                            break
+                        }
+                        break;
+                        case 3: // 3 is win tile
+                        if(i == 4){
+                            //todo win game
+                        }
+                        break;
+                        default:
+                        // jump scare
+                        break
+                    }
+                }
+            }
+               
+
+
+
 }
